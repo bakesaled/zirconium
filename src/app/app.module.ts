0e388d5af18135bdfa96578f4f44388208a14a6c
Brandon Eleuterio <brandoneleuterio@hotmail.com>
2020-03-19 11:26:41 -0600

    removing routing and shrinking music files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 import { GameComponent } from './game/game.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent, GameComponent],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
-    MatIconModule,
-    MatButtonModule
-  ],
+  imports: [BrowserModule, MatIconModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
